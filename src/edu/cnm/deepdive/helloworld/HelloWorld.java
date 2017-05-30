6aa6368a7cb8ec745f92eec2c41c3a947284412d
Sky Link <s.z_link@yahoo.com>
2017-05-30 00:10:51 -0600

    New project files, with file implementing "Hello World" program.

---
+++ src/edu/cnm/deepdive/helloworld/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package edu.cnm.deepdive.helloworld;
+
+/**
+ * @author zaryn
+ * class that prints "Hello World".
+ */
+public class HelloWorld {
+
+  /**
+   * prints "Hello World!" to standard output.
+   * @param args  command-line parameters (ignored).
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+
+}
